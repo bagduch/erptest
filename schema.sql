b957baf68edceb01bd193fbef82598fe14385258
RA Staging CLI <guy@hd.net.nz>
2016-09-06 17:07:34 +1200

    Schema update

--- schema.sql
+++ schema.sql
@@ -51,22 +51,6 @@ CREATE TABLE `mod_staffboard` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Table structure for table `password_resets`
---
-
-DROP TABLE IF EXISTS `password_resets`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `password_resets` (
-  `email` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `token` varchar(255) COLLATE utf8_unicode_ci NOT NULL,
-  `created_at` timestamp NULL DEFAULT NULL,
-  KEY `password_resets_email_index` (`email`),
-  KEY `password_resets_token_index` (`token`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
 -- Table structure for table `tblaccounts`
 --
 
