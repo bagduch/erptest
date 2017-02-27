ef7bc27baac741cb90dedbf82166e496cb2a788c
RA Staging CLI <guy@hd.net.nz>
2017-02-28 11:59:02 +1300

    Schema Commit

--- schema.sql
+++ schema.sql
@@ -102,7 +102,7 @@ CREATE TABLE `tblactivitylog` (
   `ipaddr` text NOT NULL,
   PRIMARY KEY (`id`),
   KEY `userid` (`userid`)
-) ENGINE=InnoDB AUTO_INCREMENT=133 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=156 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
