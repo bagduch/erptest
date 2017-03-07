e45fe9f774b481bd4b578e8c42a8d51ea8f5bdc9
RA Staging CLI <guy@hd.net.nz>
2017-03-07 16:59:02 +1300

    Schema Commit

--- schema.sql
+++ schema.sql
@@ -102,7 +102,7 @@ CREATE TABLE `tblactivitylog` (
   `ipaddr` text NOT NULL,
   PRIMARY KEY (`id`),
   KEY `userid` (`userid`)
-) ENGINE=InnoDB AUTO_INCREMENT=272 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=330 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -179,7 +179,7 @@ CREATE TABLE `tbladminlog` (
   KEY `logouttime` (`logouttime`),
   KEY `adminid` (`adminid`),
   CONSTRAINT `tbladminlog_ibfk_1` FOREIGN KEY (`adminid`) REFERENCES `tbladmins` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1535 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=1539 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -1164,12 +1164,15 @@ DROP TABLE IF EXISTS `tblnotes`;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `tblnotes` (
   `id` int(10) NOT NULL AUTO_INCREMENT,
+  `type` enum('order','account','productservice') DEFAULT NULL,
   `userid` int(10) NOT NULL,
   `adminid` int(10) DEFAULT NULL,
   `assignto` int(11) NOT NULL,
   `duedate` date NOT NULL,
+  `donetime` date NOT NULL,
   `note` text NOT NULL,
   `sticky` int(1) NOT NULL DEFAULT '1',
+  `flag` int(11) NOT NULL,
   `modified` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   `created` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
   PRIMARY KEY (`id`),
@@ -1177,7 +1180,7 @@ CREATE TABLE `tblnotes` (
   KEY `adminid` (`adminid`),
   CONSTRAINT `tblnotes_ibfk_1` FOREIGN KEY (`userid`) REFERENCES `tblclients` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   CONSTRAINT `tblnotes_ibfk_2` FOREIGN KEY (`adminid`) REFERENCES `tbladmins` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=104 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=107 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
