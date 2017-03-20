5c533a9fed4243e9b8a479720812129f82b225a2
RA Staging CLI <guy@hd.net.nz>
2017-03-21 01:59:10 +1300

    Schema Commit

--- schema.sql
+++ schema.sql
@@ -1,4 +1,4 @@
--- MySQL dump 10.13  Distrib 5.7.15, for Linux (x86_64)
+-- MySQL dump 10.13  Distrib 5.7.17, for Linux (x86_64)
 --
 -- Host: mysql1.hd.net.nz    Database: robotacct
 -- ------------------------------------------------------
