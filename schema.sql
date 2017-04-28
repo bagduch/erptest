0d5574c6141b0ca04838af3e05491b6e1c44f86e
RA Staging CLI <guy@hd.net.nz>
2017-04-28 12:59:02 +1200

    Schema Commit

--- schema.sql
+++ schema.sql
@@ -102,7 +102,7 @@ CREATE TABLE `tblactivitylog` (
   `ipaddr` text NOT NULL,
   PRIMARY KEY (`id`),
   KEY `userid` (`userid`)
-) ENGINE=InnoDB AUTO_INCREMENT=1226 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=1228 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -179,7 +179,7 @@ CREATE TABLE `tbladminlog` (
   KEY `logouttime` (`logouttime`),
   KEY `adminid` (`adminid`),
   CONSTRAINT `tbladminlog_ibfk_1` FOREIGN KEY (`adminid`) REFERENCES `tbladmins` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1661 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=1662 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -1550,6 +1550,7 @@ CREATE TABLE `tblservices` (
   `term` int(11) NOT NULL,
   `name` text NOT NULL,
   `stock` int(11) NOT NULL,
+  `stockalert` int(11) NOT NULL,
   `cost` float NOT NULL,
   `description` text NOT NULL,
   `revenuecode` varchar(10) DEFAULT NULL,
