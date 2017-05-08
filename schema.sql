dd47b1eac094e178de5d6873dcba2dc79e048971
RA Staging CLI <guy@hd.net.nz>
2017-05-08 12:59:02 +1200

    Schema Commit

--- schema.sql
+++ schema.sql
@@ -179,7 +179,7 @@ CREATE TABLE `tbladminlog` (
   KEY `logouttime` (`logouttime`),
   KEY `adminid` (`adminid`),
   CONSTRAINT `tbladminlog_ibfk_1` FOREIGN KEY (`adminid`) REFERENCES `tbladmins` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=1707 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=1709 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
